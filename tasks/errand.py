537db743edb92ab462a4485b27f690243ebf75df
Aues6uen11Z <79563000+Aues6uen11Z@users.noreply.github.com>
2024-07-01 20:38:48 +0800

    feat: 一键远征、一键打工

--- tasks/errand.py
+++ tasks/errand.py
@@ -13,67 +13,36 @@ class Errand(UI):
     def __init__(self, config: Dict = None):
         self.config = config
 
-    # 打工派遣
     def dispatch(self):
-        ocr = Ocr(Template(r"ERRAND_LIST.png", (0.427, 0.005)))
-        digit_ocr = DigitCounter(Template(r"ERRAND_ONIGIRI.png", (-0.037, 0.245)))
-        TPL_ERRAND_QUALITY = Template(r"ERRAND_QUALITY.png", (-0.305, 0.03))
-        target_idx = 0  # 选第几个打工，由于我的号没不符合条件的情况，所以这个功能没测试
-
-        loop_timer = Timer(0, 20).start()
-        confirm_timer = Timer(3)
+        loop_timer = Timer(0, 10).start()
         while True:
             if loop_timer.reached():
                 raise LoopError('The operation has looped too many times')
 
-            screen = self.screenshot()
-            enter_button = None
-            try:
-                enter_button = ocr.ocr_match_keyword(screen, Keyword('需要'), mode=1)[target_idx].area
-            except IndexError:
-                boxed_results = ocr.detect_and_ocr(screen)
-                if len(boxed_results) < 5:  # 列表没加载完全
-                    continue
-                elif ocr.ocr_match_keyword(screen, Keyword('剩余'), mode=1):  # 没有可派遣的打工
-                    logger.info('Errand dispatch completed')
-                    break
-            if enter_button:
-                enter_button = (enter_button[0]+enter_button[2])/2, (enter_button[1]+enter_button[3])/2
-                self.touch(enter_button, v_name='ERRAND_LIST')
-
-            self.find_click(Template(r"ERRAND_DISPATCH.png", (0.18, 0.246), Keyword('一键派遣')))
-
-            # 检查打工需要的特性是否满足
-            screen = self.screenshot()
-            quality_check = crop(screen, TPL_ERRAND_QUALITY.area)
-            if color_exists(quality_check, (255, 94, 65)):
-                # logger.info()
-                target_idx += 1
-                continue
-
-            # 检查饭团是否足够
-            if digit_ocr.ocr_single_line(screen)[1] < 0:
-                self.find_click(TPL_RETURN_BUTTON)
+            self.find_click(Template(r"QUICK_ERRAND.png", (0.283, 0.251), Keyword('一键打工')))
+            if self.find_click(Template(r"QUICK_ERRAND_CONFIRM.png", (0.141, 0.204), Keyword('一键打工'), rgb=True),
+                               times=2):
                 logger.info('Errand dispatch completed')
                 break
+            if self.exists(Template(r"ERRAND_DISABLE.png", (0.141, 0.203), rgb=True)):
+                self.find_click(Template(r"ERRAND_CANCEL.png", (-0.141, 0.204), Keyword('取消')))
+                logger.info('No place available for dispatch')
+                break
 
-            # 确保跳出界面
-            confirm_timer.reset()
-            while self.find_click(Template(r"ERRAND_START.png", (0.388, 0.245), Keyword('开始打工')), timeout=0):
-                if confirm_timer.reached():
-                    self.find_click(TPL_RETURN_BUTTON)
-                    break
-                self.sleep(0.5)
-
-    # 领前一天打工奖励
     def claim_rewards(self):
-        self.find_click(Template(r"GET_ERRAND_LIST.png", (0.48, -0.001)), timeout=0)
-        while self.find_click(Template(r"ERRAND_COMPLETE.png", (0.38, -0.248), Keyword('完成'))):
+        loop_timer = Timer(0, 10).start()
+        while True:
+            if self.exists(Template(r"ERRAND_REWARD_DONE.png", (0.42, 0.249), rgb=True)):
+                logger.info('Errand rewards claim completed')
+                break
+
+            if loop_timer.reached():
+                raise LoopError('The operation has looped too many times')
+
+            self.find_click(Template(r"ERRAND_REWARD_CLAIM.png", (0.419, 0.25), Keyword('领取奖励'), rgb=True))
             self.find_click(TPL_CONFIRM_BUTTON)
-        logger.info('Errand rewards claim completed')
 
     def run(self):
         self.ui_ensure(page_errands)
         self.claim_rewards()
         self.dispatch()
-
--- tasks/expedition.py
+++ tasks/expedition.py
@@ -14,44 +14,23 @@ class Expeditions(UI):
         self.config = config
 
     # 远征派遣
-    # TODO: 远征类型写进配置
     def dispatch(self):
-        ocr_dispatch = Ocr(Template(r"START_EXPEDITION.png", (0.239, 0.057), Keyword('开始远征')))
-        ocr_fail = Ocr(Template(r"DISPATCH_FAIL.png", (0.207, -0.004), Keyword('派遣')))
-
-        miss_count = Timer(3, 5).start()  # 防止没识别到“无法派遣”黑条进入死循环
-        loop_timer = Timer(0, 20).start()
+        loop_timer = Timer(0, 10).start()
         while True:
             if loop_timer.reached():
                 raise LoopError('The operation has looped too many times')
 
-            screen = self.screenshot()
-            start_button = None
-            try:
-                if not miss_count.reached():
-                    start_button = ocr_dispatch.ocr_match_keyword(screen, ocr_dispatch.button.keyword)[0].area
-            except IndexError:
-                boxed_results = ocr_dispatch.detect_and_ocr(screen)
-                if len(boxed_results) < 8:  # 列表没加载完全
-                    continue
-                else:  # 这一页没有可派遣的远征
-                    self.swipe(Template(r"SWIPE_START.png", (0.238, 0.207), rgb=True),
-                               Template(r"SWIPE_END.png", (0.281, -0.184)), blind1=True)
-                    continue
-
-            miss_count.reset()
-            if start_button:
-                start_button = (start_button[0] + start_button[2]) / 2, (start_button[1] + start_button[3]) / 2
-                self.touch(start_button, v_name='START_EXPEDITION')
-            self.find_click(Template(r"QUICK_DISPATCH.png", (0.119, 0.227), Keyword('派遣')), ocr_mode=1)
-            self.find_click(Template(r"DISPATCH_CONFIRM.png", (0.362, 0.227), Keyword('确定探险')))
-            self.sleep(0.3)
-            if ocr_fail.ocr_match_keyword(self.screenshot(), ocr_fail.button.keyword, mode=1):
-                self.find_click(TPL_RETURN_BUTTON, times=2)
+            self.find_click(Template(r"QUICK_EXPEDITION.png", (0.409, 0.237), Keyword('一键远征')))
+            if self.exists(Template(r"EXPEDITION_NOT_AVAILABLE.png", (0.209, -0.045), Keyword('次数不足')), ocr_mode=1):
+                self.find_click(Template(r"EXPEDITION_CANCEL.png", (-0.141, 0.205), Keyword('取消派遣')))
+                break
+            if self.find_click(Template(r"EXPEDITION_DISPATCH.png", (0.14, 0.205), Keyword('远征派遣')), times=2):
+                if self.find_click(Template(r"CLAIM_STAMINA.png", (0.092, 0.156), Keyword('取出体力')), times=2):
+                    self.find_click(Template(r"EXPEDITION_DISPATCH.png", (0.14, 0.205), Keyword('远征派遣')), times=2)
                 logger.info('Expedition dispatch completed')
                 break
 
-    # 领前一天远征奖励
+    # 领前一天远征奖励 TODO: 记录远征类型
     def claim_rewards(self):
         TPL_EXPEDITION_COMPLETED = Template(r"EXPEDITION_COMPLETED.png", (0.237, -0.09), Keyword('完成远征'))
         if not self.exists(TPL_EXPEDITION_COMPLETED, timeout=1):
