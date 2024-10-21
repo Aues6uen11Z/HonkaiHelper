1012a5a127f23b2f4be9f9b88c564fa86b4f9ab4
Aues6uen11Z <79563000+Aues6uen11Z@users.noreply.github.com>
2024-10-21 19:22:15 +0800

    fix: 正确识别没有可减负关卡

--- tasks/sweep.py
+++ tasks/sweep.py
@@ -17,19 +17,19 @@ class Sweep(UI):
         self.ui_ensure(page_battle, TPL_BATTLE_ATTACK_TAB)
         self.ui_goto(page_lite)
 
-        TPL_QUICK_LITE = Template(r"QUICK_LITE.png", (0.41, 0.241), Keyword('一键减负'))
         loop_timer = Timer(0, 10).start()
-        lite_flag = False
         while True:
             if loop_timer.reached():
                 raise LoopError('The operation has looped too many times')
 
-            if self.find_click(TPL_QUICK_LITE):
-                lite_flag = True
-            self.find_click(Template(r"LITE_BUTTON.png", (0.0, 0.133), Keyword('减负')))
-            if self.find_click(TPL_CONFIRM_BUTTON):
-                logger.info('Material sweep completed')
-                break
-            if not self.exists(TPL_QUICK_LITE) and not lite_flag:
+            find_click(Template(r"QUICK_LITE.png", (0.41, 0.241), Keyword('一键减负')))
+            if exists(Template(r"SWEEP_DONE.png", (0.002, -0.001), Keyword('没有可减负的关卡'))):
                 logger.info('Material sweep already done')
                 break
+
+            find_click(Template(r"LITE_BUTTON.png", (0.0, 0.133), Keyword('减负')))
+            if find_click(TPL_CONFIRM_BUTTON):
+                logger.info('Material sweep completed')
+                break
+
+
