4b4c6d93b73271822e46c35fa73abd83d5845751
Aues6uen11Z <79563000+Aues6uen11Z@users.noreply.github.com>
2025-02-24 17:01:33 +0800

    refactor: 修改模板布局以适应新版启动器

--- config.py
+++ config.py
@@ -2,27 +2,38 @@ import json
 from pathlib import Path
 from typing import Literal, List, Union
 
+import anyconfig
 from pydantic import BaseModel, Field
 
 
-class Argument(BaseModel):
+class Item(BaseModel):
     """
     Minimum setting
     """
-    type: Literal['input', 'select', 'checkbox'] = 'input'
+    type: Literal['input', 'select', 'checkbox', 'folder', 'file', 'priority'] = 'input'
     value: Union[str, bool, float]
     option: List[str] = []
-    hide: bool = False
+    hidden: bool = False
+    help: str = ''
+    disabled: bool = False
+
+    model_config = {
+        'populate_by_name': True,
+    }
+
+    def __init__(self, value=None, **data):
+        if value is not None and not isinstance(value, dict):
+            data['value'] = value
+        super().__init__(**data)
 
 
 class GroupCustomBase(BaseModel):
     """
     Basic settings for every task
     """
-    priority: int = 3
-    priority_enabled: bool = True
-    command: str = ''
-    command_enabled: bool = True
+    # active: Item = Item(type='checkbox', value=True)
+    priority: Item = Item(type='priority', value=0)
+    command: Item = Item('')
 
 
 # 以下是实际设置内容
@@ -32,116 +43,95 @@ class TaskGeneral(BaseModel):
         """
         General settings for the project
         """
-        work_dir: str = './examples/HonkaiHelper'
-        work_dir_enabled: bool = True
-        is_background: bool = False
-        is_background_enabled: bool = True
-        config_path: str = './examples/HonkaiHelper/config/config.json'
-        config_path_enabled: bool = True
+        language: Item = Item('中文')
+        work_dir: Item = Item('./repos/HonkaiHelper')
+        background: Item = Item(value=False, disabled=True)
+        config_path: Item = Item('./repos/HonkaiHelper/config/config.json')
 
     class GroupGame(BaseModel):
-        game_path: Argument = Argument(type='input', value='')
-        log_retain: Argument = Argument(type='select', value='1week', option=['1day', '3days', '1week', '1month'])
+        game_path: Item = Item(type='input', value='')
+        log_retain: Item = Item(type='select', value='1week', option=['1day', '3days', '1week', '1month'])
 
     Base: GroupGeneralBase = Field(GroupGeneralBase(), alias='_Base')
     Game: GroupGame = GroupGame()
 
 
-class TaskUpdate(BaseModel):
-    class GroupUpdateBase(BaseModel):
-        """
-        Git repository, python virtual environment update settings
-        """
-        repo_url: str = 'https://gitee.com/aues6uen11z/HonkaiHelper'
-        repo_url_enabled: bool = True
-        branch: str = 'master'
-        branch_enabled: bool = True
-        local_path: str = './examples/HonkaiHelper'
-        local_path_enabled: bool = True
-        template_path: str = 'config'
-        template_path_enabled: bool = True
-        env_name: str = 'zafkiel'
-        pip_mirror: str = 'https://pypi.tuna.tsinghua.edu.cn/simple'
-
-    Base: GroupUpdateBase = Field(GroupUpdateBase(), alias='_Base')
-
-
 class TaskArmada(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t armada', priority=5
+        command=Item('py main.py -t armada'), priority=Item(4)
     ), alias='_Base')
 
 
 class TaskDormBonus(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t dorm_bonus', priority=2
+        command=Item('py main.py -t dorm_bonus'), priority=Item(5)
     ), alias='_Base')
 
 
 class TaskErrand(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t errand', priority=3
+        command=Item('py main.py -t errand'), priority=Item(5)
     ), alias='_Base')
 
 
 class TaskExpedition(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t expedition', priority=4
+        command=Item('py main.py -t expedition'), priority=Item(5)
     ), alias='_Base')
 
 
 class TaskLogin(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t login', priority=0, priority_enabled=False
+        command=Item('py main.py -t login'), priority=Item(value=31, disabled=True)
     ), alias='_Base')
 
 
 class TaskLogout(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t logout', priority=100, priority_enabled=False
+        command=Item('py main.py -t logout'), priority=Item(value=0, disabled=True)
     ), alias='_Base')
 
 
 class TaskMail(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t mail', priority=7
+        command=Item('py main.py -t mail'), priority=Item(value=4)
     ), alias='_Base')
 
 
 class TaskMission1(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t mission', priority=1, priority_enabled=False
+        command=Item('py main.py -t mission'), priority=Item(value=6, disabled=True)
     ), alias='_Base')
 
 
 class TaskMission2(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t mission', priority=8, priority_enabled=False
+        command=Item('py main.py -t mission'), priority=Item(value=2, disabled=True)
     ), alias='_Base')
 
 
 class TaskSweep(BaseModel):
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t sweep', priority=6
+        command=Item('py main.py -t sweep'), priority=Item(3)
     ), alias='_Base')
 
 
 class TaskWeeklyReward(BaseModel):
     class GroupWeeklyEvent(BaseModel):
-        share: Argument = Argument(type='checkbox', value=True)
-        share_time: Argument = Argument(type='input', value=0.0, hide=True)
+        share: Item = Item(type='checkbox', value=True)
+        share_time: Item = Item(type='input', value=0.0, hidden=True)
 
-        homo_chest: Argument = Argument(type='checkbox', value=True)
-        homo_chest_time: Argument = Argument(type='input', value=0.0, hide=True)
+        homo_chest: Item = Item(type='checkbox', value=True)
+        homo_chest_time: Item = Item(type='input', value=0.0, hidden=True)
 
-        bp_chest: Argument = Argument(type='checkbox', value=True)
-        bp_chest_time: Argument = Argument(type='input', value=0.0, hide=True)
+        bp_chest: Item = Item(type='checkbox', value=True)
+        bp_chest_time: Item = Item(type='input', value=0.0, hidden=True)
 
-        armada_contribution: Argument = Argument(type='checkbox', value=True)
-        armada_contribution_time: Argument = Argument(type='input', value=0.0, hide=True)
+        armada_contribution: Item = Item(type='checkbox', value=True)
+        armada_contribution_time: Item = Item(type='input', value=0.0, hidden=True)
 
     Base: GroupCustomBase = Field(GroupCustomBase(
-        command='py main.py -t weekly_reward', priority=9
+        command=Item('py main.py -t weekly_reward'), priority=Item(1)
     ), alias='_Base')
     WeeklyEvent: GroupWeeklyEvent = GroupWeeklyEvent()
 
@@ -149,7 +139,6 @@ class TaskWeeklyReward(BaseModel):
 # 任务组级别
 class MenuProject(BaseModel):
     General: TaskGeneral = TaskGeneral()
-    Update: TaskUpdate = TaskUpdate()
 
 
 class MenuDaily(BaseModel):
@@ -176,58 +165,102 @@ class UIContent(BaseModel):
     Weekly: MenuWeekly = MenuWeekly()
 
 
-def gen_i18n(config: BaseModel, lang: str):
-    trans_dict = {
-        "Menu": {},
-        "Task": {},
+def gen_i18n(lang: str):
+    trans_path = f"./config/i18n/{lang}.json"
+    template_path = "./config/template.json"
+
+    trans = {
+        "Project": {
+            "tasks": {
+                "General": {
+                    "groups": {}
+                }
+            }
+        }
     }
+    with open(template_path, 'r', encoding='utf-8') as f:
+        tpl = anyconfig.load(f)
 
-    # Check if the file already exists
-    file_path = f'./config/i18n/{lang}.json'
-    if Path(file_path).exists():
-        # Load the existing translations
-        with open(file_path, 'r', encoding='utf-8') as f:
-            old_trans_dict = json.load(f)
-    else:
-        old_trans_dict = {}
+    for menu_name, menu_conf in tpl.items():
+        if menu_name == "Project":
+            if "General" in menu_conf.keys():
 
-    for menu, tasks in config.model_dump(by_alias=True).items():
-        trans_dict['Menu'][menu] = old_trans_dict.get('Menu', {}).get(menu, {'name': ''})
+                group_trans = trans["Project"]["tasks"]["General"]["groups"]
+                for group_name, group_conf in menu_conf["General"].items():
+                    if group_name == "_Base":
+                        continue
+                    group_trans[group_name] = {
+                            "name": group_name,
+                            "help": group_conf.get("_help", {}).get("value", ""),
+                            "items": {}
+                        }
+                    
+                    item_trans = group_trans[group_name]["items"]
+                    for item_name, item_conf in group_conf.items():
+                        if item_name == "_help":
+                            continue
+                        item_trans[item_name] = {
+                                "name": item_name,
+                                "help": item_conf.get("help", ""),
+                            }
+                        for option_name in item_conf.get("option", []):
+                            item_trans[item_name].setdefault("options", {})[option_name] = option_name
+        else:
+            trans[menu_name] = {
+                "name": menu_name,
+                "tasks": {}
+            }
 
-        for task, groups in tasks.items():
-            trans_dict['Task'][task] = old_trans_dict.get('Task', {}).get(task, {'name': ''})
+            task_trans = trans[menu_name]["tasks"]
+            for task_name, task_conf in menu_conf.items():
+                task_trans[task_name] = {
+                    "name": task_name,
+                    "groups": {}
+                }
 
-            for group, args in groups.items():
-                if group == '_Base':
-                    continue
-                if group not in trans_dict:
-                    trans_dict[group] = {}
+                group_trans = task_trans[task_name]["groups"]
+                for group_name, group_conf in task_conf.items():
+                    if group_name == "_Base":
+                        continue
+                    group_trans[group_name] = {
+                        "name": group_name,
+                        "help": group_conf.get("_help", {}).get("value", ""),
+                        "items": {}
+                    }
 
-                trans_dict[group]['_info'] = old_trans_dict.get(group, {}).get('_info', {'name': '', 'help': ''})
-                for arg, info in args.items():
-                    trans_dict[group][arg] = old_trans_dict.get(group, {}).get(arg, {'name': '', 'help': ''})
-                    if info['type'] == 'select':
-                        for option in info['option']:
-                            trans_dict[group][arg][option] = old_trans_dict.get(group, {}).get(arg, {}).get(option, '')
+                    item_trans = group_trans[group_name]["items"]
+                    for item_name, item_conf in group_conf.items():
+                        if item_name == "_help":
+                            continue
+                        item_trans[item_name] = {
+                            "name": item_name,
+                            "help": item_conf.get("help", "")
+                        }
+                        for option_name in item_conf.get("option", []):
+                            item_trans[item_name].setdefault("options", {})[option_name] = option_name
 
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(trans_dict, f, ensure_ascii=False, indent=2)
+    if Path(trans_path).exists():
+        with open(trans_path, 'r', encoding='utf-8') as f:
+            old_trans = anyconfig.load(f)
+        anyconfig.merge(trans, old_trans)
+
+    with open(trans_path, 'w', encoding='utf-8') as f:
+        anyconfig.dump(trans, f, ensure_ascii=False, indent=2, allow_unicode=True)
+
 
 
 def export() -> None:
     args = UIContent()
-    with open('config/args.json', 'w') as f:
+    with open('config/template.json', 'w', encoding='utf-8') as f:
         f.write(args.model_dump_json(indent=2, by_alias=True))
-
-    gen_i18n(args, 'zh-CN')
 
 
 class Config:
     def __init__(self, config_path):
         self.config_path = config_path
-        with open('config/args.json', 'r') as f:
+        with open('config/template.json', 'r', encoding='utf-8') as f:
             args = json.load(f)
-        with open(config_path, 'r') as f:
+        with open(config_path, 'r', encoding='utf-8') as f:
             self.data = json.load(f)
 
         # 只是为了校验数据
@@ -236,15 +269,16 @@ class Config:
                 for group, args in groups.items():
                     if group == '_Base':
                         continue
-                    for argument, info in args.items():
-                        info['value'] = self.data[task][group][argument]
-        UIContent.parse_obj(args)
+                    for Item, info in args.items():
+                        info['value'] = self.data[task][group][Item]
+        UIContent.model_validate(args)
 
-    def update(self, task, group, argument, value):
-        self.data[task][group][argument] = value
+    def update(self, task, group, Item, value):
+        self.data[task][group][Item] = value
         with open(self.config_path, 'w') as f:
             json.dump(self.data, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == '__main__':
-    export()
+    # export()
+    gen_i18n('中文')
