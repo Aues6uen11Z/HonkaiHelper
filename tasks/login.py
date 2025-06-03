6e79cece4bf5eb29a6d2f9127fe3d3d5304a1420
Aues6uen11Z <79563000+Aues6uen11Z@users.noreply.github.com>
2025-06-03 22:21:09 +0800

    fix: 领取每周作战历练箱

--- tasks/login.py
+++ tasks/login.py
@@ -4,7 +4,17 @@ import time
 from pathlib import Path
 import subprocess
 
-from zafkiel import Template, logger, wait, touch, stop_app, auto_setup, sleep, exists, find_click
+from zafkiel import (
+    Template,
+    logger,
+    wait,
+    touch,
+    stop_app,
+    auto_setup,
+    sleep,
+    exists,
+    find_click,
+)
 from zafkiel.ocr import Keyword
 from zafkiel.ui import UI
 
@@ -19,15 +29,17 @@ class Login(UI):
 
     def manage_log(self):
         log_retain_map = {
-            '1day': 1,
-            '3days': 3,
-            '1week': 7,
-            '1month': 30,
+            "1day": 1,
+            "3days": 3,
+            "1week": 7,
+            "1month": 30,
         }
-        retain_days = log_retain_map.get(self.config.data['Project']['General']['Game']['log_retain'], 7)
+        retain_days = log_retain_map.get(
+            self.config.data["Project"]["General"]["Game"]["log_retain"], 7
+        )
 
         current_time = time.time()
-        log_path = Path('./log')
+        log_path = Path("./log")
 
         for log_dir in log_path.iterdir():
             create_time = log_dir.stat().st_ctime
@@ -35,22 +47,29 @@ class Login(UI):
 
             if age_in_days > retain_days:
                 try:
-                    logger.info(f'Deleting old log directory: {log_dir}')
+                    logger.info(f"Deleting old log directory: {log_dir}")
                     shutil.rmtree(log_dir)
                 except Exception as e:
-                    logger.error(f'Failed to delete {log_dir}: {e}')
+                    logger.error(f"Failed to delete {log_dir}: {e}")
 
     def handle_app_login(self):
-
-        wait(Template(r"LOGIN_FLAG.png", (0.406, 0.233), rgb=True),
-             timeout=1200, interval=3, interval_func=self.check_update)
+        wait(
+            Template(r"LOGIN_FLAG.png", (0.406, 0.233), rgb=True),
+            timeout=1200,
+            interval=3,
+            interval_func=self.check_update,
+        )
         touch(Template(r"LOGIN_CLICK.png", (-0.002, -0.031)), times=2, blind=True)
 
         try:
-            confirm_time = float(self.config.data['Daily']['Login']['Login']['confirm_time'])
+            confirm_time = float(
+                self.config.data["Daily"]["Login"]["Login"]["confirm_time"]
+            )
             if confirm_time < 3.0:
                 confirm_time = 3.0
-                logger.warning("Confirm time was less than 3.0, setting to minimum value of 3.0")
+                logger.warning(
+                    "Confirm time was less than 3.0, setting to minimum value of 3.0"
+                )
         except:
             confirm_time = 3.0
             logger.warning("Invalid confirm_time in config, using default value of 3.0")
@@ -63,7 +82,7 @@ class Login(UI):
             if self.ui_page_appear(page_main):
                 sleep(confirm_time)
                 if not self.ui_ensure(page_main):
-                    logger.info('Game login successful')
+                    logger.info("Game login successful")
                     break
 
         return True
@@ -72,11 +91,17 @@ class Login(UI):
         stop_app()
 
     def app_start(self):
-        subprocess.Popen([self.config.data['Project']['General']['Game']['game_path']])
+        subprocess.Popen([self.config.data["Project"]["General"]["Game"]["game_path"]])
         date = datetime.datetime.now().strftime("%Y-%m-%d")
-        auto_setup(str(Path.cwd()), logdir=f'./log/{date}/report', devices=["WindowsPlatform:///?title=崩坏3", ])
+        auto_setup(
+            str(Path.cwd()),
+            logdir=f"./log/{date}/report",
+            devices=[
+                "WindowsPlatform:///?title=崩坏3",
+            ],
+        )
         self.manage_log()
-        self.get_popup_list(popup_list)  # TODO: Move to program start instead of game start
+        self.get_popup_list(popup_list)
 
         sleep(15)
         self.handle_app_login()
@@ -90,6 +115,9 @@ class Login(UI):
     def check_update():
         if exists(Template(r"LOGIN_UPDATE.png", (0.002, -0.129))):
             find_click(Template(r"DOWNLOAD_CONFIRM.png", (0.0, 0.116)))
-            logger.info('Game updating')
-            if find_click(Template(r"DOWNLOAD_DONE.png", (0.0, 0.048), Keyword('确定')), timeout=1200):
-                logger.info('Game update completed')
+            logger.info("Game updating")
+            if find_click(
+                Template(r"DOWNLOAD_DONE.png", (0.0, 0.048), Keyword("确定")),
+                timeout=1200,
+            ):
+                logger.info("Game update completed")
--- tasks/weekly_reward.py
+++ tasks/weekly_reward.py
@@ -6,8 +6,14 @@ from zafkiel.ocr import Keyword
 from zafkiel.ui import UI
 
 from config import Config
-from tasks.base.page import TPL_RETURN_BUTTON, page_missions, TPL_CONFIRM_BUTTON, page_main, TPL_NEW_ITEM, \
-    page_armada_contribution
+from tasks.base.page import (
+    TPL_RETURN_BUTTON,
+    page_missions,
+    TPL_CONFIRM_BUTTON,
+    page_main,
+    TPL_NEW_ITEM,
+    page_armada_contribution,
+)
 from tasks.base.popup import popup_handler
 from tasks.base.switch import TPL_BP_MISSIONS_TAB, TPL_BP_REWARDS_TAB
 
@@ -18,91 +24,133 @@ class WeeklyReward(UI):
 
         now = datetime.datetime.now()
         monday = now - datetime.timedelta(days=now.weekday())
-        self.monday_4am = monday.replace(hour=4, minute=0, second=0, microsecond=0).timestamp()
+        self.monday_4am = monday.replace(
+            hour=4, minute=0, second=0, microsecond=0
+        ).timestamp()
 
     def share(self):
-        if not self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['share']:
+        if not self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"]["share"]:
             return
 
-        if self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['share_time'] > self.monday_4am:
-            logger.info('Weekly sharing already completed')
+        if (
+            self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"]["share_time"]
+            > self.monday_4am
+        ):
+            logger.info("Weekly sharing already completed")
             return
 
         self.ui_ensure(page_missions, TPL_BP_MISSIONS_TAB)
-        if exists(Template(r"WEEKLY_SHARE.png", (-0.196, -0.134), Keyword('每周分享'))):
-            find_click(Template(r"GOTO_SHARE.png", (0.013, -0.083), Keyword('前往')))
+        if exists(Template(r"WEEKLY_SHARE.png", (-0.196, -0.134), Keyword("每周分享"))):
+            find_click(Template(r"GOTO_SHARE.png", (0.013, -0.083), Keyword("前往")))
             touch(Template(r"SHARE_ITEM.png", (-0.199, -0.095)), blind=True)
             find_click(Template(r"SHARE_BUTTON.png", (-0.35, 0.222)), times=2)
             find_click(TPL_RETURN_BUTTON)
-            logger.info('Weekly sharing completed')
+            logger.info("Weekly sharing completed")
         else:
             # 若已经手动完成
-            logger.info('Weekly sharing already completed')
-        self.config.update('Weekly', 'WeeklyReward', 'WeeklyEvent', 'share_time', time.time())
+            logger.info("Weekly sharing already completed")
+        self.config.update(
+            "Weekly", "WeeklyReward", "WeeklyEvent", "share_time", time.time()
+        )
 
     def homo_chest(self):
-        if not self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['homo_chest']:
+        if not self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"]["homo_chest"]:
             return
 
-        if self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['homo_chest_time'] > self.monday_4am:
-            logger.info('Weekly homo chest already claimed')
+        if (
+            self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"]["homo_chest_time"]
+            > self.monday_4am
+        ):
+            logger.info("Weekly homo chest already claimed")
             return
 
         self.ui_ensure(page_main)
         find_click(Template(r"MAIN_GOTO_MALL.png", (-0.423, 0.076)))
-        find_click(Template(r"GOTO_GIFT.png", (-0.355, -0.123), Keyword('礼包')))
-        find_click(Template(r"GOTO_WEEKLY_GIFT.png", (-0.355, -0.005), Keyword('周期')))
-        if find_click(Template(r"BUY_HOMO_CHEST.png", (-0.207, 0.041), Keyword('免费'))):
-            find_click(Template(r"HOMO_CHEST_CONFIRM.png", (-0.002, 0.18), Keyword('免费')))
+        find_click(Template(r"GOTO_GIFT.png", (-0.355, -0.123), Keyword("礼包")))
+        find_click(Template(r"GOTO_WEEKLY_GIFT.png", (-0.355, -0.005), Keyword("周期")))
+        if find_click(
+            Template(r"BUY_HOMO_CHEST.png", (-0.207, 0.041), Keyword("免费"))
+        ):
+            find_click(
+                Template(r"HOMO_CHEST_CONFIRM.png", (-0.002, 0.18), Keyword("免费"))
+            )
             find_click(TPL_CONFIRM_BUTTON)
-            logger.info('Weekly homo chest claim completed')
+            logger.info("Weekly homo chest claim completed")
         else:
-            logger.info('Weekly homo chest already claimed')
-        self.config.update('Weekly', 'WeeklyReward', 'WeeklyEvent', 'homo_chest_time', time.time())
+            logger.info("Weekly homo chest already claimed")
+        self.config.update(
+            "Weekly", "WeeklyReward", "WeeklyEvent", "homo_chest_time", time.time()
+        )
         find_click(TPL_RETURN_BUTTON)
 
     def bp_chest(self):
-        if not self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['bp_chest']:
+        if not self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"]["bp_chest"]:
             return
 
-        if self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['bp_chest_time'] > self.monday_4am:
-            logger.info('Weekly bp chest already claimed')
+        if (
+            self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"]["bp_chest_time"]
+            > self.monday_4am
+        ):
+            logger.info("Weekly bp chest already claimed")
             return
 
         self.ui_ensure(page_missions, TPL_BP_REWARDS_TAB)
-        if find_click(Template(r"BP_CHEST.png", (0.334, 0.203))):
+        if find_click(
+            Template(r"BP_CHEST_TEXT.png", (0.411, 0.197), Keyword("作战历练箱")),
+            Template(r"BP_CHEST.png", (0.334, 0.203)),
+            blind=True,
+        ):
             find_click(TPL_NEW_ITEM, timeout=2)
             find_click(TPL_CONFIRM_BUTTON)
             popup_handler.handle_bp_reward()
-            logger.info('Weekly bp chest claim completed')
+            logger.info("Weekly bp chest claim completed")
         else:
-            logger.info('Weekly bp chest already claimed')
-        self.config.update('Weekly', 'WeeklyReward', 'WeeklyEvent', 'bp_chest_time', time.time())
+            logger.info("Weekly bp chest already claimed")
+        self.config.update(
+            "Weekly", "WeeklyReward", "WeeklyEvent", "bp_chest_time", time.time()
+        )
 
     def armada_contribution_reward(self):
-        if not self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['armada_contribution']:
+        if not self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"][
+            "armada_contribution"
+        ]:
             return
 
-        if self.config.data['Weekly']['WeeklyReward']['WeeklyEvent']['armada_contribution_time'] > self.monday_4am:
-            logger.info('Armada contribution reward already claimed')
+        if (
+            self.config.data["Weekly"]["WeeklyReward"]["WeeklyEvent"][
+                "armada_contribution_time"
+            ]
+            > self.monday_4am
+        ):
+            logger.info("Armada contribution reward already claimed")
             return
 
         self.ui_ensure(page_armada_contribution)
         if exists(Template(r"CONTRIBUTION_FULL.png", (-0.325, -0.123))):
             find_click(Template(r"CONTRIBUTION_REWARD.png", (-0.442, -0.122)))
             find_click(TPL_CONFIRM_BUTTON)
-            self.config.update('Weekly', 'WeeklyReward', 'WeeklyEvent', 'armada_contribution_time', time.time())
-            logger.info('Armada contribution reward claim completed')
+            self.config.update(
+                "Weekly",
+                "WeeklyReward",
+                "WeeklyEvent",
+                "armada_contribution_time",
+                time.time(),
+            )
+            logger.info("Armada contribution reward claim completed")
         elif exists(Template(r"CONTRIBUTION_CLAIMED.png", (-0.324, -0.122))):
-            self.config.update('Weekly', 'WeeklyReward', 'WeeklyEvent', 'armada_contribution_time', time.time())
-            logger.info('Armada contribution reward already claimed')
+            self.config.update(
+                "Weekly",
+                "WeeklyReward",
+                "WeeklyEvent",
+                "armada_contribution_time",
+                time.time(),
+            )
+            logger.info("Armada contribution reward already claimed")
         else:
-            logger.info('Armada contribution reward not available')
+            logger.info("Armada contribution reward not available")
 
     def run(self):
         self.share()
         self.homo_chest()
         self.bp_chest()
         self.armada_contribution_reward()
-
-
